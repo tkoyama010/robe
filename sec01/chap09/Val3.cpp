ab77765c8552b80fa173001748de991626b108e1
Tetsuo Koyama <tkoyama010@gmail.com>
2019-01-02 21:49:20 +0900

    Add Val3.cpp

---
+++ sec01/chap09/Val3.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+int a = 0;
+
+void Test()
+{
+    int b = 1;
+    std::cout << a << b << c << d << std::endl;
+    return;
+}
+
+int c = 2;
+
+int main()
+{
+    int d = 3;
+    Test();
+    std::cout << a << b << c << d << std::endl;
+    return 0;
+}
